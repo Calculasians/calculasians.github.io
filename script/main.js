08841b3e257d59572a5e95324eaf119a7a50e888
Daniel Sun <hy.dsun55012@gmail.com>
2019-06-20 16:52:56 -0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
 	window.onscroll = windowScrolled;
     	function windowScrolled() {
-        if (window.pageYOffset >= $(window).height() && window.pageYOffset < $(window).height() + $("#about-page").height()) {
-            $(".nav-button").css("font-weight", "lighter");
-            $("#nav-about").css("font-weight", "bolder");
+	if (window.pageYOffset <$(window).height()) {
+		$(".nav-button").css("font-weight", "lighter");
+	} else if (window.pageYOffset >= $(window).height() && window.pageYOffset < $(window).height() + $("#about-page").height()) {
+            	$(".nav-button").css("font-weight", "lighter");
+            	$("#nav-about").css("font-weight", "bolder");
         } else if (window.pageYOffset >= $(window).height() + $("#about-page").height() && 
                    window.pageYOffset < $(window).height() + $("#about-page").height() + $("#experience").height()) {
-            $(".nav-button").css("font-weight", "lighter");                 
-            $("#nav-projects").css("font-weight", "bolder");
-        } else {
-        		$(".nav-button").css("font-weight", "lighter");
-            $("#nav-writings").css("font-weight", "bolder");
-            
+            	$(".nav-button").css("font-weight", "lighter");                 
+            	$("#nav-experience").css("font-weight", "bolder");
+        } else if (window.pageYOffset >= $(window).height() + $("#about-page").height() + $("#experience").height() &&
+		   window.pageYOffset < $(window).height() + $("#about-page").height() + $("#experience").height() + $("#projects").height()) {
+	   	$(".nav-button").css("font-weight", "lighter");        
+		$("#nav-projects").css("font-weight", "bolder");     
+	} else if (window.pageYOffset >= $(window).height() + $("#about-page").height() + $("#experience").height() + $("#projects").height() &&
+		   window.pageYOffset < $(window).height() + $("#about-page").height() + $("#experience").height() + $("#projects").height() + $("#hobbies").height()) {   
+		$(".nav-button").css("font-weight", "lighter");     
+		$("#nav-hobbies").css("font-weight", "bolder");     
+	} else {
+        	$(".nav-button").css("font-weight", "lighter");
+            	$("#nav-writings").css("font-weight", "bolder");
         }
     }	
 });
