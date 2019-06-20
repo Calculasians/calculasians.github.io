c14d7bdb75ed90ad06afa5ed5f26eb7a31b187b9
Daniel Sun <hy.dsun55012@gmail.com>
2019-06-20 16:10:24 -0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,1 +1,11 @@
-
+window.onscroll = windowScrolled;
+    function windowScrolled() {
+        if (window.pageYOffset >= $(window).height() && window.pageYOffset < $(window).height() + $("#about-page").height()) {
+            $("#nav-about").css("font-weight", "bolder");
+        } else if (window.pageYOffset >= $(window).height() + $("#about-page").height() && 
+                   window.pageYOffset < $(window).height() + $("#about-page").height() + $("#experience").height()) {
+            $("#nav-projects").css("font-weight", "bolder");
+        } else {
+            $("#nav-writings").css("font-weight", "bolder");
+        }
+    }
