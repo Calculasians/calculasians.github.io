e0524dde8208b8b3bebfc6df253316a7c59d3740
Daniel Sun <hy.dsun55012@gmail.com>
2019-06-23 12:54:36 -0700

    make hobbies bold when scrolled

--- script/main.js
+++ script/main.js
@@ -16,22 +16,16 @@ $(document).ready(function() {
             	$("#nav-experience").css("font-weight", "bolder");
 		$("#nav-experience").css("margin-left", "6px");
         } else if (window.pageYOffset >= $(window).height() + $("#about-page").height() + $("#experience").height() &&
-		   window.pageYOffset < $(window).height() + $("#about-page").height() + $("#experience").height() + $("#projects").height()) {
+		   window.pageYOffset < $(window).height() + $("#about-page").height() + $("#experience").height() + $("#projects").height() - 70) {
 	   	$(".nav-button").css("font-weight", "lighter");    
 		$(".nav-button").css("margin-left", "0px");
 		$("#nav-projects").css("font-weight", "bolder");    
 		$("#nav-projects").css("margin-left", "6px");
-	} else if (window.pageYOffset >= $(window).height() + $("#about-page").height() + $("#experience").height() + $("#projects").height() &&
-		   window.pageYOffset < $(window).height() + $("#about-page").height() + $("#experience").height() + $("#projects").height() + $("#hobbies").height()) {   
+	} else {
 		$(".nav-button").css("font-weight", "lighter");   
 		$(".nav-button").css("margin-left", "0px");
 		$("#nav-hobbies").css("font-weight", "bolder");   
 		$("#nav-hobbies").css("margin-left", "6px");
-	} else {
-        	$(".nav-button").css("font-weight", "lighter");
-		$(".nav-button").css("margin-left", "0px");
-            	$("#nav-writings").css("font-weight", "bolder");
-		$("#nav-writings").css("margin-left", "6px");
-        }
+	}
     }	
 });
